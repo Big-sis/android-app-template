7b05629f2abffe7204c93f9cf2204c519170f208
Clairehere <b_claire@hotmail.com>
2018-05-16 16:22:10 +0200

    rectif main en connexion

--- app/src/main/java/fr/wildcodeschool/vyfe/ConnexionActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/ConnexionActivity.java
@@ -3,11 +3,11 @@ package fr.wildcodeschool.vyfe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ConnexionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connexion);
     }
 }
