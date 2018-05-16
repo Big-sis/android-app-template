876433314875ab004a3afddcb6d43b1abfa9e984
HugoCalas <hugo.calas31@gmail.com>
2018-05-16 16:40:06 +0200

    layout 6, 7, et 7bis

---
+++ app/src/main/java/fr/wildcodeschool/vyfe/InfoVideo.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.vyfe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_video);
+    }
+}
---
+++ app/src/main/java/fr/wildcodeschool/vyfe/SelectedVideo.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.vyfe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectedVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_video);
+    }
+}
