9d252861f7118eb653e4c3612a40725955f32772
Perrine <morandperrine@hotmail.fr>
2018-05-23 11:40:16 +0200

    rajout du activity_connextion

--- app/src/main/java/fr/wildcodeschool/vyfe/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/MainActivity.java
@@ -2,6 +2,7 @@ package fr.wildcodeschool.vyfe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
     }
 }
