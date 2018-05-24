664de7aad83c7e70e04d84f010940d6aced9cac7
Clairehere <b_claire@hotmail.com>
2018-05-24 11:02:04 +0200

    choix couleur

--- app/src/main/java/fr/wildcodeschool/vyfe/AddGridActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/AddGridActivity.java
@@ -8,7 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Random;
+
+import petrov.kristiyan.colorpicker.ColorPicker;
 
 public class AddGridActivity extends AppCompatActivity {
 
@@ -17,6 +20,14 @@ public class AddGridActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grid);
         final EditText etName = findViewById(R.id.et_name);
+
+        Button btnChooseColor = findViewById(R.id.btn_chosse_color);
+        btnChooseColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openColorChoose();
+            }
+        });
 
         final Button btnAleatory = findViewById(R.id.btn_aleatory);
         btnAleatory.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +39,50 @@ public class AddGridActivity extends AppCompatActivity {
                 etName.setBackgroundColor(color);
             }
         });
+    }
+
+    public void openColorChoose() {
+        final ColorPicker colorPicker = new ColorPicker(this);
+        final ArrayList<String> colors = new ArrayList<>();
+        //les couleurs seront insérées dans values avec la charte graphique
+        colors.add(("#bd000d"));
+        colors.add(("#5a00c5"));
+        colors.add(("#1e8900"));
+        colors.add(("#b7bb00"));
+        colors.add(("#b664ba"));
+
+
+        colors.add(("#f91734"));
+        colors.add(("#932bf9"));
+        colors.add(("#5bba25"));
+        colors.add(("#eded42"));
+        colors.add(("#ea94ed"));
+
+        colors.add(("#ff5f5e"));
+        colors.add(("#ca62ff"));
+        colors.add(("#90ed59"));
+        colors.add(("#ffff77"));
+        colors.add(("#ffc6ff"));
+
+
+        colorPicker.setColors(colors)
+                .setColumns(5)
+                .setRoundColorButton(true)
+                .setTitle("Choisir votre couleur")
+                .setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
+                    @Override
+                    public void onChooseColor(int position, int color) {
+                        EditText etName = findViewById(R.id.et_name);
+                        etName.setBackgroundColor(color);
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+                }).show();
+
+
     }
 }
