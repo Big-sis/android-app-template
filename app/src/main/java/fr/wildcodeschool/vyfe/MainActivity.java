bb91495ee49419c4c53b0cf2b5d095157c700320
Perrine <morandperrine@hotmail.fr>
2018-05-24 11:34:15 +0200

    changement de la toolbar

--- app/src/main/java/fr/wildcodeschool/vyfe/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/MainActivity.java
@@ -2,6 +2,7 @@ package fr.wildcodeschool.vyfe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // Texte en dur pour l'exemple, il sera retiré plus tard
+        getSupportActionBar().setTitle("Hello");
     }
 
     @Override
