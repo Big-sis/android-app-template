833e467b8b6b2c9a9874ce3b143bd15796d25cd7
Clairehere <b_claire@hotmail.com>
2018-05-24 10:51:12 +0200

    couleur aléatoireé

--- app/src/main/java/fr/wildcodeschool/vyfe/AddGridActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/AddGridActivity.java
@@ -1,7 +1,14 @@
 package fr.wildcodeschool.vyfe;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class AddGridActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class AddGridActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grid);
+        final EditText etName = findViewById(R.id.et_name);
+
+        final Button btnAleatory = findViewById(R.id.btn_aleatory);
+        btnAleatory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random random = new Random();
+                int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+                etName.setBackgroundColor(color);
+            }
+        });
     }
 }
