17b82affc62b50608ad9fce37497154647ef3826
Clairehere <b_claire@hotmail.com>
2018-05-25 12:03:41 +0200

    grid evenement en parcelable

--- app/src/main/java/fr/wildcodeschool/vyfe/ObservationItemsModel.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/ObservationItemsModel.java
@@ -1,6 +1,9 @@
 package fr.wildcodeschool.vyfe;
 
-public class ObservationItemsModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ObservationItemsModel implements Parcelable {
     private int color;
     private String name;
 
@@ -8,6 +11,23 @@ public class ObservationItemsModel {
         this.color = color;
         this.name = name;
     }
+
+    protected ObservationItemsModel(Parcel in) {
+        color = in.readInt();
+        name = in.readString();
+    }
+
+    public static final Creator<ObservationItemsModel> CREATOR = new Creator<ObservationItemsModel>() {
+        @Override
+        public ObservationItemsModel createFromParcel(Parcel in) {
+            return new ObservationItemsModel(in);
+        }
+
+        @Override
+        public ObservationItemsModel[] newArray(int size) {
+            return new ObservationItemsModel[size];
+        }
+    };
 
     public int getColor() {
         return color;
@@ -23,5 +43,16 @@ public class ObservationItemsModel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(color);
+        parcel.writeString(name);
     }
 }
