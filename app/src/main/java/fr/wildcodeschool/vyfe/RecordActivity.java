5bd25d3c94a9a16bd22a6cba644bd0033f5fad8c
HugoCalas <hugo.calas31@gmail.com>
2018-05-25 11:39:03 +0200

    modif listes RecordActivity

--- app/src/main/java/fr/wildcodeschool/vyfe/RecordActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/RecordActivity.java
@@ -2,6 +2,10 @@ package fr.wildcodeschool.vyfe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class RecordActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class RecordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        RecyclerView listItems = findViewById(R.id.list_tags);
+
+        final ArrayList<ObservationItemsModel> tagsList = new ArrayList<>();
+        //insertion des observations a ajouter
+        tagsList.add(new ObservationItemsModel(0, "Se gratte la tête"));
+        tagsList.add(new ObservationItemsModel(0, "Silence"));
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        listItems.setLayoutManager(layoutManager);
+
+        final ObservationsRecyclerAdapter adapter = new ObservationsRecyclerAdapter(tagsList);
+        listItems.setAdapter(adapter);
+
+
+
     }
 }
