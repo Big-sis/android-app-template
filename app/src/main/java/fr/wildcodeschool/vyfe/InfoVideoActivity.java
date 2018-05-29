8d6b0e01341a066596e0b3faca509da102354454
Clairehere <b_claire@hotmail.com>
2018-05-29 14:54:17 +0200

    spinner dans infosVideoActivity

--- app/src/main/java/fr/wildcodeschool/vyfe/InfoVideoActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/InfoVideoActivity.java
@@ -2,6 +2,8 @@ package fr.wildcodeschool.vyfe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class InfoVideoActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class InfoVideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_video);
+
+        final Spinner spinner=findViewById(R.id.spin_folder);
+        //creer array utiliser un adapterSpinner pour rentrer les donner du spinner arrays
+        final ArrayAdapter<CharSequence> adapterSpinner=ArrayAdapter.createFromResource(this, R.array.select_folder, android.R.layout.simple_spinner_item);
+        //specifier le layout a utiliser lors affichage donné
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //appliquer ladapter au spinner
+        spinner.setAdapter(adapterSpinner);
     }
 }
