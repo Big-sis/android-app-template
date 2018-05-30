b8e2d15309677aa4421e2af1bd93d259ee534c35
Perrine <morandperrine@hotmail.fr>
2018-05-30 14:39:23 +0200

    design mes vidéos

--- app/src/main/java/fr/wildcodeschool/vyfe/MyVideoActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/MyVideoActivity.java
@@ -2,6 +2,8 @@ package fr.wildcodeschool.vyfe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MyVideoActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MyVideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_video);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.my_videos);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
     }
 }
