cfe2ad328569fc3971e32d8ec2193dcb24fae732
Perrine <morandperrine@hotmail.fr>
2018-06-01 15:40:14 +0200

    possibilité de filmer depuis l'application

--- app/src/main/java/fr/wildcodeschool/vyfe/RecordActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/RecordActivity.java
@@ -1,7 +1,14 @@
 package fr.wildcodeschool.vyfe;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,12 +16,19 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.VideoView;
 
 import java.util.ArrayList;
 
 public class RecordActivity extends AppCompatActivity {
 
-    ArrayList<TagModel> mTagModels = new ArrayList<>();
+    private ArrayList<TagModel> mTagModels = new ArrayList<>();
+    private boolean mPermission;
+    private VideoView mVideoView;
+
+    public static final int RECORD_VIDEO = 1;
+    public static final int PERMISSION_REQUEST = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,16 @@ public class RecordActivity extends AppCompatActivity {
         recyclerTags.setAdapter(adapterTags);
         recyclerTime.setAdapter(adapterTime);
 
+        final FloatingActionButton btnRecord = findViewById(R.id.bt_record_stop);
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnRecord.setBackgroundColor(getResources().getColor(R.color.colorRosyPink));
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(intent, RECORD_VIDEO);
+            }
+        });
 
         FloatingActionButton btFinish = findViewById(R.id.bt_finish);
         btFinish.setOnClickListener(new View.OnClickListener() {
@@ -54,11 +78,25 @@ public class RecordActivity extends AppCompatActivity {
             }
         });
 
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            mPermission = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.settings, menu);
         return true;
+    }
+
+    protected void onActivityResult(int request, int result, Intent intent) {
+        if (request == RECORD_VIDEO && result == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            mVideoView.setVideoURI(videoUri);
+        }
     }
 }
