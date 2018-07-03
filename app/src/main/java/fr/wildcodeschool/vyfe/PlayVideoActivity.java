cfd50917c7da43a6f691f255f56bc38ac0eb8f78
HugoCalas <hugo.calas31@gmail.com>
2018-07-03 13:48:02 +0200

    merge

--- app/src/main/java/fr/wildcodeschool/vyfe/PlayVideoActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/PlayVideoActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Pair;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
