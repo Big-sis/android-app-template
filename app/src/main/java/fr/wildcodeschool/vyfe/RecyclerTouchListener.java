cb0ae5b55189535d0b70ba233c8c800e2e22c7eb
Vyfe <vyfe@MacBook-Pro-de-chloe.local>
2018-08-21 15:16:41 +0200

    gestion click: effets luminosité

--- app/src/main/java/fr/wildcodeschool/vyfe/RecyclerTouchListener.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/RecyclerTouchListener.java
@@ -6,16 +6,35 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.logging.Handler;
+
 public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
 
     private GestureDetector gestureDetector;
     private ClickListener clickListener;
 
-    public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+
+    public RecyclerTouchListener(final Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
         this.clickListener = clickListener;
         gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onSingleTapUp(MotionEvent e) {
+                final View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                if (child != null && clickListener != null) {
+                    clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    child.setAlpha((float) 0.3);
+
+                    recyclerView.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            child.setAlpha(1);
+                        }
+                    },600);
+
+
+                }
+
                 return true;
             }
 
@@ -24,6 +43,14 @@ public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
                 View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
                 if (child != null && clickListener != null) {
                     clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    child.setAlpha((float) 0.3);
+
+                    recyclerView.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            child.setAlpha(1);
+                        }
+                    },600);
                 }
             }
         });
