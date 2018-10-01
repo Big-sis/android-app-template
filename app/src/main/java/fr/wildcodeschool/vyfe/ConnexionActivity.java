70a7ef2a1f70c37e0cacf4450d462cb4d0a12044
Clairehere <b_claire@hotmail.com>
2018-10-01 15:54:46 +0200

    String en anglais

--- app/src/main/java/fr/wildcodeschool/vyfe/ConnexionActivity.java
+++ app/src/main/java/fr/wildcodeschool/vyfe/ConnexionActivity.java
@@ -2,13 +2,17 @@ package fr.wildcodeschool.vyfe;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,28 +43,7 @@ public class ConnexionActivity extends AppCompatActivity {
         final EditText inputPass = findViewById(R.id.et_password);
         Button forgotPassword = findViewById(R.id.tv_lost_password);
         final TextView btnCreateAccount = findViewById(R.id.btn_create_account);
-        final ImageView ivFlag = findViewById(R.id.iv_flag);
-        final boolean[] frenchFlag = {true};
-        final String[] language = {""};
 
-
-        ivFlag.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (frenchFlag[0]) {
-                    language[0] = "en";
-                    ivFlag.setBackgroundResource(R.drawable.icons8_grande_bretagne_96);
-                    frenchFlag[0] = false;
-
-                } else {
-                    language[0] = "fr";
-                    ivFlag.setBackgroundResource(R.drawable.icons8_france_96);
-                    frenchFlag[0] = true;
-                }
-                //TODO: implementer le string-en (en attente traduction Alex)
-                ChangeLanguage(language[0]);
-            }
-        });
 
         Button connexion = findViewById(R.id.btn_connected);
         connexion.setOnClickListener(new View.OnClickListener() {
@@ -129,12 +112,4 @@ public class ConnexionActivity extends AppCompatActivity {
 
     }
 
-    private void ChangeLanguage(String language) {
-        Locale locale = new Locale(language);
-        Locale.setDefault(locale);
-        Configuration config = new Configuration();
-        config.locale = locale;
-        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-
-    }
 }
